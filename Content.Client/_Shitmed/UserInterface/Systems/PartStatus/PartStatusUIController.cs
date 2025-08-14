f7e520ab89e4d0c5670a84e75526681bccec7abf
JrInventor05Next <dorokhovartem2010@gmail.com>
2025-08-14 23:39:00 +0700

    Update PartStatusUIController.cs

--- Content.Client/_Shitmed/UserInterface/Systems/PartStatus/PartStatusUIController.cs
+++ Content.Client/_Shitmed/UserInterface/Systems/PartStatus/PartStatusUIController.cs
@@ -84,7 +84,7 @@ public sealed class PartStatusUIController : UIController, IOnStateEntered<Gamep
     public void UpdatePartStatusControl(EntityUid entity, TargetingComponent component)
     {
         if (EntityManager.HasComponent<PainNumbnessComponent>(entity))
-            PartStatusControl.SetTextures(_spriteSystem.Frame0);
+            return _spriteSystem.Frame0();
         else if (PartStatusControl != null && _targetingComponent != null)
             PartStatusControl.SetTextures(_targetingComponent.BodyStatus);
     }
